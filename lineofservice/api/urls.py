ac10a8f6c5fb08f6e078d30f58d2bc337de3bc9d
Joseph Serunjogi <joeseggie@gmail.com>
2019-07-26 23:40:10 +0300

    Line of service routes

---
+++ lineofservice/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from lineofservice.api.views import LineOfServiceViewSet
+
+router = DefaultRouter()
+router.register(r'linesofservice', LineOfServiceViewSet)
+
+urlpatterns = [
+    path('v0.1', include(router.urls)),
+]
