2680dae649b816efed87353a6654cb2d8757a92c
Joseph Serunjogi <joeseggie@gmail.com>
2019-07-26 22:13:28 +0300

    Client model

---
+++ client/models.py
@@ -0,0 +1,35 @@
+import re
+
+from django.db import models
+
+from client_industry.models import ClientIndustry
+from common.models import BaseModel
+from organization.models import Organization
+
+
+class Client(BaseModel):
+    """Client model"""
+
+    name = models.CharField(max_length=256, null=False)
+    name_slug = models.CharField(max_length=256,
+                                 editable=False,
+                                 null=False,
+                                 blank=False,
+                                 unique=True)
+    address = models.CharField(max_length=256)
+    client_industry = models.ForeignKey(ClientIndustry,
+                                        null=True,
+                                        on_delete=models.SET_NULL)
+    organization = models.ForeignKey(Organization,
+                                     null=True,
+                                     on_delete=models.SET_NULL)
+
+    class Meta:
+        db_table = 'client',
+
+    def save(self, *args, **kwargs):
+        self.name_slug = re.sub(r'\W', '-', self.name.lower())
+        super(Client, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
