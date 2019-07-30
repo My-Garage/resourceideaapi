1cfb838030cd2d17e7f3550165d59429ebb8a039
Joseph Serunjogi <joeseggie@gmail.com>
2019-07-30 22:47:46 +0300

    Task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from common.models import BaseModel
+from organization.models import Organization
+from job.models import Job
+from common.enums import ProgressStatus
 
-# Create your models here.
+
+class Task(BaseModel):
+    """Model that holds information about tasks."""
+
+    class Meta:
+        db_table = 'task'
+
+    title = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    status = models.TextField(
+        max_length=20,
+        default=ProgressStatus.NOT_STARTED.value,
+        choices=[(status.value, status.value) for status in ProgressStatus])
+    organization = models.ForeignKey(Organization,
+                                     null=True,
+                                     on_delete=models.SET_NULL,
+                                     blank=True)
+    job = models.ForeignKey(Job,
+                            null=True,
+                            on_delete=models.SET_NULL,
+                            blank=True)
+
+    def __str__(self):
+        return self.title
