224bfcc2cd0b8efd12934ffe0a3d3486ca7f5057
Joseph Serunjogi <joeseggie@gmail.com>
2019-07-30 23:28:07 +0300

    Task assignment model

--- task_assignment/models.py
+++ task_assignment/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from common.models import BaseModel
+from task.models import Task
+from employee.models import Employee
 
-# Create your models here.
+
+class TaskAssignment(BaseModel):
+    """Model that holds information about task assignments."""
+
+    class Meta:
+        db_table = 'task_assignment'
+
+    start_date_time = models.DateTimeField()
+    completion_date_time = models.DateTimeField()
+    task = models.ForeignKey(Task,
+                             null=True,
+                             on_delete=models.SET_NULL)
+    employee = models.ForeignKey(Employee,
+                                 null=True,
+                                 on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return '%s - %s' % (str(self.task), str(self.employee))
