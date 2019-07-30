16406ac2b332bf412331843729f2c1a6451ac3c7
Joseph Serunjogi <joeseggie@gmail.com>
2019-07-30 13:44:42 +0300

    Progress status enumeration

--- common/enums.py
+++ common/enums.py
@@ -6,3 +6,13 @@ class Status(enum.Enum):
     ACTIVE = 'active'
     DISABLED = 'disabled'
     ARCHIVED = 'archived'
+
+
+class ProgressStatus(enum.Enum):
+    """Progress status enumeration."""
+
+    NOT_STARTED = 'not started',
+    RUNNING = 'running'
+    IN_REVIEW = 'in review'
+    REVIEWED = 'reviewed'
+    CLOSED = 'closed'
