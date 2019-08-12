915d501f382a822ebf72a320353451fc1cf30576
Joseph Serunjogi <joeseggie@gmail.com>
2019-08-12 15:23:39 +0300

    Setting permissions for view
    Permitted user in the organization should be allowed to perform actions.

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.urls import include
 
 from common.views import index
-
 
 urlpatterns = [
     path('initialsetup/', index, name='initialsetup'),
--- employee/api/views.py
+++ employee/api/views.py
@@ -3,13 +3,22 @@ from rest_framework import viewsets
 
 from employee.api.serializers import EmployeeSerializer
 from employee.models import Employee
+from common.permissions import IsOrganizationAdministrator
 
 
 class EmployeeViewSet(mixins.UpdateModelMixin,
                       mixins.ListModelMixin,
                       mixins.RetrieveModelMixin,
+                      mixins.CreateModelMixin,
                       viewsets.GenericViewSet):
     """Employee view set"""
 
-    queryset = Employee.objects.all()
+    queryset = Employee.objects.none()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsOrganizationAdministrator, ]
+
+    def get_queryset(self):
+        return Employee.objects\
+            .filter(
+                organization_id=self.request.user.employee.organization_id)\
+            .all()
