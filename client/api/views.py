237170c8613fd6821894710f9806efb6f8b52c4b
Joseph Serunjogi <joeseggie@gmail.com>
2019-08-19 19:27:23 +0300

    Setting the queryset for the client view

--- client/api/views.py
+++ client/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 
 from client.api.serializers import ClientSerializer
 from client.models import Client
+from client.permissions import ClientPermissions
 
 
 class ClientViewSet(mixins.CreateModelMixin,
@@ -12,5 +13,11 @@ class ClientViewSet(mixins.CreateModelMixin,
                     viewsets.GenericViewSet):
     """Client view set"""
 
-    queryset = Client.objects.all()
+    queryset = Client.objects.none()
     serializer_class = ClientSerializer
+    permission_classes = [ClientPermissions]
+
+    def get_queryset(self):
+        return Client.objects.filter(
+            organization_id=self.request.user.employee.organization_id)\
+            .all()
