ce070dd3f3076b6aeb01410f1fdf510f553b1258
Joseph Serunjogi <joeseggie@gmail.com>
2019-08-19 15:32:39 +0300

    Setting the queryset for engagement views

--- engagement/api/views.py
+++ engagement/api/views.py
@@ -12,6 +12,11 @@ class EngagementViewSet(mixins.CreateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet):
 
-    queryset = Engagement.objects.all()
+    queryset = Engagement.objects.none()
     serializer_class = EngagementSerializer
     permission_classes = [EngagementPermissions]
+
+    def get_queryset(self):
+        return Engagement.objects.filter(
+            organization_id=self.request.user.employee.organization_id)\
+            .all()
