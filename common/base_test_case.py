ce914a98660654b3ffc597fd2e04f8aedb1fbcbf
Joseph Serunjogi <joeseggie@gmail.com>
2019-08-19 13:26:43 +0300

    Testing engagement app
    Testing of the engagement app routes.

---
+++ common/base_test_case.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from common.utils import create_resource_groups
+from common.utils import create_administrator_groups
+from employee.models import Employee
+from organization.models import Organization
+
+
+class BaseAPITestCase(APITestCase):
+    user_username = 'testuser'
+    user_password = 'strongkey'
+
+    def setUp(self):
+        create_administrator_groups()
+        create_resource_groups()
+        self.user = User.objects.create_user(username=self.user_username,
+                                             password=self.user_password,
+                                             first_name='Serunjogi',
+                                             last_name='Joseph')
+        self.user.is_active = True
+        self.user.save()
+        self.organization = Organization.objects.create(name='Test Org',
+                                                        status='active')
+        response = self.client.post(
+            '/api/token/',
+            data={'username': self.user_username,
+                  'password': self.user_password})
+        self.access_token = response.data['access']
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
