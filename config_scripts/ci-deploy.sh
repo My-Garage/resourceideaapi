725d291ed767cebec9f52cc2ca5a19a44ae7ea90
Scott Businge <busingescott@gmail.com>
2020-07-28 00:46:40 +0300

    solve auth erro on deployment update

--- config_scripts/ci-deploy.sh
+++ config_scripts/ci-deploy.sh
@@ -16,7 +16,7 @@ mv ./k8s/deployment.yml.out ./k8s/deployment.yml
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 ./kubectl \
-  ----insecure-skip-tls-verify \
+  --insecure-skip-tls-verify \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
   # --certificate-authority=cert.crt \
