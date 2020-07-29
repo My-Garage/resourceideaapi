5f9d5468496094c725b74973415ccc75d27e0bd8
Scott Businge <busingescott@gmail.com>
2020-07-29 18:14:52 +0300

    pass ci

--- config_scripts/ci-deploy.sh
+++ config_scripts/ci-deploy.sh
@@ -16,4 +16,4 @@ mv ./k8s/deployment.yml.out ./k8s/deployment.yml
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
 # ./kubectl --kubeconfig=/dev/null --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN get pods -n resourceideaapi
-./kubectl --insecure-skip-tls-verify --kubeconfig="/dev/null" --server=$KUBERNETES_SERVER --token=$KUBERNETES_TOKEN get pods -n resourceideaapi
+./kubectl --insecure-skip-tls-verify --kubeconfig="/dev/null" --server=$KUBERNETES_SERVER --certificate-authority=cert.crt --token=$KUBERNETES_TOKEN get pods -n resourceideaapi
